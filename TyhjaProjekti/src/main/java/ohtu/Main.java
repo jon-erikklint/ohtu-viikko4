012be5d90157379963753f90c8ff2bb950005e04
jon-erikklint <jonerik.klint@gmail.com>
2017-04-06 13:45:58 +0300

    1 ja 2

--- TyhjaProjekti/src/main/java/ohtu/Main.java
+++ TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,11 +1,19 @@
 package ohtu;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.http.client.fluent.Request;
 
 public class Main {
 
     public static void main(String[] args) {
+        Main main = new Main();
+        main.work(args);
+    }
+    
+    public void work(String[] args){
         String studentNr = "012345678";
         
         
@@ -13,8 +21,22 @@ public class Main {
             studentNr = args[0];
         }
 
-        String url = "http://ohtustats2017.herokuapp.com/students/"+studentNr+"/submissions";
+        String studentUrl = "http://ohtustats2017.herokuapp.com/students/"+studentNr+"/submissions";
+        String courseUrl = "https://ohtustats2017.herokuapp.com/courses/1.json";
 
+        Submission[] subs = (Submission[]) fetch(studentUrl, Submission[].class);
+        Course course = (Course) fetch(courseUrl, Course.class);
+        
+        tulostaKurssinTiedot(course);
+        tulostaOpiskelijanTiedot(subs, studentNr);
+    }
+    
+    private Object fetch(String url, Class made){
+        String bodyText = getBodyText(url);
+        return unpack(bodyText, made);
+    }
+    
+    private String getBodyText(String url){
         String bodyText;
         
         try{
@@ -22,18 +44,32 @@ public class Main {
         }catch(Exception e){
             bodyText = "";
         }
-         
-
-        System.out.println("json-muotoinen data:");
-        System.out.println( bodyText );
-
-        Gson mapper = new Gson();
-        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
         
-        System.out.println("Oliot:");
+        return bodyText;
+    }
+    
+    private Object unpack(String bodyText, Class clas){
+        Gson mapper = new Gson();
+        Object object = mapper.fromJson(bodyText, clas);
+        
+        return object;
+    }
+    
+    private void tulostaOpiskelijanTiedot(Submission[] subs, String studentNr){
+        System.out.println("opiskelijanumero: "+studentNr+"\n");
+        int tehdytTehtavat = 0;
+        int kaytettyAika = 0;
+        
         for (Submission submission : subs) {
             System.out.println(submission);
+            tehdytTehtavat += submission.tehtavamaara();
+            kaytettyAika += submission.getHours();
         }
         
+        System.out.println("\nyhteensä: "+tehdytTehtavat+" tehtävää "+kaytettyAika+" tuntia");
+    }
+    
+    private void tulostaKurssinTiedot(Course course){
+        System.out.println("Kurssi: "+course+"\n");   
     }
 }
--- TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -5,8 +5,26 @@ public class Submission {
     private int week;
     private int hours;
     
-    private boolean[] excercises = new boolean[21];
     private boolean a1;
+    private boolean a2;
+    private boolean a3;
+    private boolean a4;
+    private boolean a5;
+    private boolean a6;
+    private boolean a7;
+    private boolean a8;
+    private boolean a9;
+    private boolean a10;
+    private boolean a11;
+    private boolean a12;
+    private boolean a13;
+    private boolean a14;
+    private boolean a15;
+    private boolean a16;
+    private boolean a17;
+    private boolean a18;
+    private boolean a19;
+    private boolean a20;
 
     public String getStudent_number() {
         return student_number;
@@ -31,182 +49,219 @@ public class Submission {
     public void setHours(int hours) {
         this.hours = hours;
     }
-    
-    private void setExcercise(int i, boolean done){
-        excercises[i] = done;
+
+    public boolean isA1() {
+        return a1;
+    }
+
+    public void setA1(boolean a1) {
+        this.a1 = a1;
+    }
+
+    public boolean isA2() {
+        return a2;
+    }
+
+    public void setA2(boolean a2) {
+        this.a2 = a2;
+    }
+
+    public boolean isA3() {
+        return a3;
+    }
+
+    public void setA3(boolean a3) {
+        this.a3 = a3;
+    }
+
+    public boolean isA4() {
+        return a4;
+    }
+
+    public void setA4(boolean a4) {
+        this.a4 = a4;
+    }
+
+    public boolean isA5() {
+        return a5;
+    }
+
+    public void setA5(boolean a5) {
+        this.a5 = a5;
+    }
+
+    public boolean isA6() {
+        return a6;
+    }
+
+    public void setA6(boolean a6) {
+        this.a6 = a6;
+    }
+
+    public boolean isA7() {
+        return a7;
+    }
+
+    public void setA7(boolean a7) {
+        this.a7 = a7;
+    }
+
+    public boolean isA8() {
+        return a8;
+    }
+
+    public void setA8(boolean a8) {
+        this.a8 = a8;
+    }
+
+    public boolean isA9() {
+        return a9;
+    }
+
+    public void setA9(boolean a9) {
+        this.a9 = a9;
+    }
+
+    public boolean isA10() {
+        return a10;
+    }
+
+    public void setA10(boolean a10) {
+        this.a10 = a10;
+    }
+
+    public boolean isA11() {
+        return a11;
+    }
+
+    public void setA11(boolean a11) {
+        this.a11 = a11;
+    }
+
+    public boolean isA12() {
+        return a12;
+    }
+
+    public void setA12(boolean a12) {
+        this.a12 = a12;
+    }
+
+    public boolean isA13() {
+        return a13;
+    }
+
+    public void setA13(boolean a13) {
+        this.a13 = a13;
+    }
+
+    public boolean isA14() {
+        return a14;
+    }
+
+    public void setA14(boolean a14) {
+        this.a14 = a14;
+    }
+
+    public boolean isA15() {
+        return a15;
+    }
+
+    public void setA15(boolean a15) {
+        this.a15 = a15;
+    }
+
+    public boolean isA16() {
+        return a16;
+    }
+
+    public void setA16(boolean a16) {
+        this.a16 = a16;
+    }
+
+    public boolean isA17() {
+        return a17;
+    }
+
+    public void setA17(boolean a17) {
+        this.a17 = a17;
+    }
+
+    public boolean isA18() {
+        return a18;
+    }
+
+    public void setA18(boolean a18) {
+        this.a18 = a18;
+    }
+
+    public boolean isA19() {
+        return a19;
+    }
+
+    public void setA19(boolean a19) {
+        this.a19 = a19;
+    }
+
+    public boolean isA20() {
+        return a20;
+    }
+
+    public void setA20(boolean a20) {
+        this.a20 = a20;
     }
     
-    private boolean getExcercise(int i){
-        return excercises[i];
+    
+    public int tehtavamaara(){
+        int amount = 0;
+        if(a1) amount++;
+        if(a2) amount++;
+        if(a3) amount++;
+        if(a4) amount++;
+        if(a5) amount++;
+        if(a6) amount++;
+        if(a7) amount++;
+        if(a8) amount++;
+        if(a9) amount++;
+        if(a10) amount++;
+        if(a11) amount++;
+        if(a12) amount++;
+        if(a13) amount++;
+        if(a14) amount++;
+        if(a15) amount++;
+        if(a16) amount++;
+        if(a17) amount++;
+        if(a18) amount++;
+        if(a19) amount++;
+        if(a20) amount++;
+        
+        return amount;
     }
     
-    public boolean getA1(){
-        return getExcercise(0);
-    }
-    
-    public boolean getA2(){
-        return getExcercise(1);
-    }
-    
-    public boolean getA3(){
-        return getExcercise(2);
-    }
-    
-    public boolean getA4(){
-        return getExcercise(3);
-    }
-    
-    public boolean getA5(){
-        return getExcercise(4);
-    }
-    
-    public boolean getA6(){
-        return getExcercise(5);
-    }
-    
-    public boolean getA7(){
-        return getExcercise(6);
-    }
-    
-    public boolean getA8(){
-        return getExcercise(7);
-    }
-    
-    public boolean getA9(){
-        return getExcercise(8);
-    }
-    
-    public boolean getA10(){
-        return getExcercise(9);
-    }
-    
-    public boolean getA11(){
-        return getExcercise(10);
-    }
-    
-    public boolean getA12(){
-        return getExcercise(11);
-    }
-    
-    public boolean getA13(){
-        return getExcercise(12);
-    }
-    
-    public boolean getA14(){
-        return getExcercise(13);
-    }
-    
-    public boolean getA15(){
-        return getExcercise(14);
-    }
-    
-    public boolean getA16(){
-        return getExcercise(15);
-    }
-    
-    public boolean getA17(){
-        return getExcercise(16);
-    }
-    
-    public boolean getA18(){
-        return getExcercise(17);
-    }
-    
-    public boolean getA19(){
-        return getExcercise(18);
-    }
-    
-    public boolean getA20(){
-        return getExcercise(19);
-    }
-    
-    public boolean getA21(){
-        return getExcercise(20);
-    }
-    
-    public void setA1(boolean done){
-        System.out.println("sad :(");
-        setExcercise(0, done);
-    }
-    
-    public void setA2(boolean done){
-        setExcercise(1, done);
-    }
-    
-    public void setA3(boolean done){
-        setExcercise(2, done);
-    }
-    
-    public void setA4(boolean done){
-        setExcercise(3, done);
-    }
-    
-    public void setA5(boolean done){
-        setExcercise(4, done);
-    }
-    
-    public void setA6(boolean done){
-        setExcercise(5, done);
-    }
-    
-    public void setA7(boolean done){
-        setExcercise(6, done);
-    }
-    
-    public void setA8(boolean done){
-        setExcercise(7, done);
-    }
-    
-    public void setA9(boolean done){
-        setExcercise(8, done);
-    }
-    
-    public void setA10(boolean done){
-        setExcercise(9, done);
-    }
-    
-    public void setA11(boolean done){
-        setExcercise(10, done);
-    }
-    
-    public void setA12(boolean done){
-        setExcercise(11, done);
-    }
-    
-    public void setA13(boolean done){
-        setExcercise(12, done);
-    }
-    
-    public void setA14(boolean done){
-        setExcercise(13, done);
-    }
-    
-    public void setA15(boolean done){
-        setExcercise(14, done);
-    }
-    
-    public void setA16(boolean done){
-        setExcercise(15, done);
-    }
-    
-    public void setA17(boolean done){
-        setExcercise(16, done);
-    }
-    
-    public void setA18(boolean done){
-        setExcercise(17, done);
-    }
-    
-    public void setA19(boolean done){
-        setExcercise(18, done);
-    }
-    
-    public void setA20(boolean done){
-        setExcercise(19, done);
-    }
-    
-    public void setA21(boolean done){
-        setExcercise(20, done);
+    public String tehdytTehtavat(){
+        StringBuilder builder = new StringBuilder();
+        
+        if(a1) builder.append("1 ");
+        if(a2) builder.append("2 ");
+        if(a3) builder.append("3 ");
+        if(a4) builder.append("4 ");
+        if(a5) builder.append("5 ");
+        if(a6) builder.append("6 ");
+        if(a7) builder.append("7 ");
+        if(a8) builder.append("8 ");
+        if(a9) builder.append("9 ");
+        if(a10) builder.append("10 ");
+        if(a11) builder.append("11 ");
+        if(a12) builder.append("12 ");
+        if(a13) builder.append("13 ");
+        if(a14) builder.append("14 ");
+        if(a15) builder.append("15 ");
+        if(a16) builder.append("16 ");
+        if(a17) builder.append("17 ");
+        if(a18) builder.append("18 ");
+        if(a19) builder.append("19 ");
+        if(a20) builder.append("21 ");
+        
+        return builder.toString();
     }
 
     @Override
@@ -214,20 +269,8 @@ public class Submission {
         StringBuilder palautus = new StringBuilder();
         palautus.append("viikko "+week+": tehtyjä tehtäviä yhteensä: ");
         
-        int doneAmount = 0;
-        StringBuilder done = new StringBuilder();
-        
-        for (int i = 0; i < excercises.length; i++) {
-            System.out.println(excercises[i]);
-            
-            if(excercises[i]){
-                doneAmount++;
-                done.append(i).append(" ");
-            }
-        }
-        
-        palautus.append(doneAmount).append(" aikaa kului: ").append(hours)
-                .append(", tehdyt tehtävät: ").append(done);
+        palautus.append(tehtavamaara()).append(" aikaa kului: ").append(hours)
+                .append(", tehdyt tehtävät: ").append(tehdytTehtavat());
         
         return palautus.toString();
     }
