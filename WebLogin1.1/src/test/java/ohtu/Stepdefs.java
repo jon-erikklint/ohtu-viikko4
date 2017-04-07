ee9e05a25ed88ac212e2666a8b90d33216c220ae
jon-erikklint <jonerik.klint@gmail.com>
2017-04-07 17:32:34 +0300

    7

--- WebLogin1.1/src/test/java/ohtu/Stepdefs.java
+++ WebLogin1.1/src/test/java/ohtu/Stepdefs.java
@@ -6,20 +6,34 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import static org.junit.Assert.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Stepdefs {
-    WebDriver driver = new HtmlUnitDriver();
+    WebDriver driver = new ChromeDriver();
     String baseUrl = "http://localhost:4567";
-
-
+    
     @Given("^login is selected$")
     public void login_selected() throws Throwable {
         driver.get(baseUrl);
         WebElement element = driver.findElement(By.linkText("login"));       
         element.click();          
+    } 
+
+    @When("^username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
+    public void username_and_password_are_given(String username, String password) throws Throwable {
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("login"));
+        element.submit();  
+    }
+
+    @Then("^system will respond \"([^\"]*)\"$")
+    public void system_will_respond(String pageContent) throws Throwable {
+        assertTrue(driver.getPageSource().contains(pageContent));
     }
     
     @When("^correct username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
@@ -27,14 +41,8 @@ public class Stepdefs {
         logInWith(username, password);
     }
 
-    
     @When("^correct username \"([^\"]*)\" and incorrect password \"([^\"]*)\" are given$")
     public void username_and_incorrect_password_are_given(String username, String password) throws Throwable {
-        logInWith(username, password);
-    }
-    
-    @When("^incorrect username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
-    public void incorrect_username_and_password_are_given(String username, String password) throws Throwable {
         logInWith(username, password);
     }
     
@@ -47,13 +55,14 @@ public class Stepdefs {
     public void user_is_not_logged_in_and_error_message_is_given() throws Throwable {
         pageHasContent("invalid username or password");
         pageHasContent("Give your credentials to login");
-    }   
-
+    }     
+    
     @After
     public void tearDown(){
         driver.quit();
     }
         
+    /* helper methods */
  
     private void pageHasContent(String content) {
         assertTrue(driver.getPageSource().contains(content));
@@ -68,5 +77,4 @@ public class Stepdefs {
         element = driver.findElement(By.name("login"));
         element.submit();  
     } 
-    
 }
--- WebLogin1.1/src/test/resources/ohtu/logging_in.feature
+++ WebLogin1.1/src/test/resources/ohtu/logging_in.feature
@@ -2,10 +2,10 @@ Feature: As a registered user can log in with valid username/password-combinatio
 
 Scenario: user can login with correct password
 Given login is selected
-When username "jukka" and password "akkuj" are given
-Then system will respond "Ohtu Application main page"
+When correct username "jukka" and password "akkuj" are given
+Then user is logged in
 
 Scenario: user can not login with incorrect password
 Given login is selected
-When username "jukka" and password "wrong" are given
-Then system will respond "invalid username or password"
+When correct username "jukka" and incorrect password "wrong" are given
+Then user is not logged in and error message is given
